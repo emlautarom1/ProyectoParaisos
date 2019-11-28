11598de5d4d88717ab41bf0103b18c599ee5c33e
Lautaro Martin Emanuel <emlautarom1@gmail.com>
2019-11-28 17:35:28 -0300

    Simplified routing

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -9,43 +9,23 @@ const routes: Routes = [
     children: [
       {
         path: 'region-map',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('./region-map/region-map.module').then(m => m.RegionMapPageModule)
-          }
-        ]
+        loadChildren: () =>
+          import('./region-map/region-map.module').then(m => m.RegionMapPageModule)
       },
       {
         path: 'observation',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('./observation/observation.module').then(m => m.ObservationPageModule)
-          }
-        ]
+        loadChildren: () =>
+          import('./observation/observation.module').then(m => m.ObservationPageModule)
       },
       {
         path: 'wiki',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('./wiki/wiki.module').then(m => m.WikiPageModule)
-          }
-        ]
+        loadChildren: () =>
+          import('./wiki/wiki.module').then(m => m.WikiPageModule)
       },
       {
         path: 'account',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('./account/account.module').then(m => m.AccountPageModule)
-          }
-        ]
+        loadChildren: () =>
+          import('./account/account.module').then(m => m.AccountPageModule)
       }
     ]
   },
