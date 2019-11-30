cc771b11675af61862113c96257db1ad42359308
Lautaro Martin Emanuel <emlautarom1@gmail.com>
2019-11-30 01:24:57 -0300

    Implemented 2 steps: Location and Pictures

--- src/app/tabs/observation/observation.page.ts
+++ src/app/tabs/observation/observation.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { GeolocationService } from 'src/app/services/geolocation.service';
 
 @Component({
@@ -7,7 +7,11 @@ import { GeolocationService } from 'src/app/services/geolocation.service';
   styleUrls: ['./observation.page.scss'],
 })
 export class ObservationPage implements OnInit {
+  @ViewChild('imginput', { static: false }) imageInput: ElementRef;
+
+  currentStep = 1;
   coords: Coordinates;
+  picture: string | ArrayBuffer;
 
   constructor(private geo: GeolocationService) { }
 
@@ -17,4 +21,36 @@ export class ObservationPage implements OnInit {
     });
   }
 
+  getCurrentTitle() {
+    switch (this.currentStep) {
+      case 1: return "Tu ubicación actual";
+      case 2: return "Toma unas Fotos"
+      default: return `<ERROR: Paso ${this.currentStep}>`
+    }
+  }
+
+  onGoBackClick() {
+    if (this.currentStep == 2) {
+      this.currentStep--;
+    }
+  }
+
+  onContinueClick() {
+    if (this.currentStep == 1) {
+      this.currentStep++
+    }
+  }
+
+  onInputChange(files: FileList) {
+    if (files && files.length) {
+      // Should be responsability of "PicturesService"
+      const reader = new FileReader();
+      reader.onload = (event: ProgressEvent) => {
+        console.log(event);
+
+        this.picture = (event.target as any).result;
+      };
+      reader.readAsDataURL(files[0]);
+    }
+  }
 }
