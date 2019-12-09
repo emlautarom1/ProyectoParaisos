9edb3981a4b846de0a7e5ea1419d2c96e5ab9f5c
Lautaro Martin Emanuel <emlautarom1@gmail.com>
2019-12-09 14:57:30 -0300

    Added step 3 to observation form

--- src/app/tabs/observation/observation.page.ts
+++ src/app/tabs/observation/observation.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { GeolocationService } from 'src/app/services/geolocation.service';
+import { DateService } from 'src/app/services/date.service';
+import { ObservationFormService } from 'src/app/services/observation-form.service';
 
 @Component({
   selector: 'app-observation',
@@ -9,34 +11,44 @@ import { GeolocationService } from 'src/app/services/geolocation.service';
 export class ObservationPage implements OnInit {
   @ViewChild('imginput', { static: false }) imageInput: ElementRef;
 
-  currentStep = 1;
-  coords: Coordinates;
-  picture: string | ArrayBuffer;
+  currentStep = 3;
 
-  constructor(private geo: GeolocationService) { }
+  coords: Coordinates;
+  picture: String | ArrayBuffer;
+  address: String;
+  date: String;
+
+  constructor(
+    private geoS: GeolocationService,
+    private dateS: DateService,
+  ) { }
 
   ngOnInit() {
-    this.geo.watchLocation().subscribe((pos: Position) => {
+    this.geoS.watchLocation().subscribe((pos: Position) => {
       this.coords = pos.coords;
+      this.geoS.coordsToAddress(pos.coords)
+        .subscribe(addr => this.address = addr);
     });
+    this.date = this.dateS.getCurrentDate();
   }
 
   getCurrentTitle() {
     switch (this.currentStep) {
       case 1: return "Tu ubicación actual";
-      case 2: return "Toma unas Fotos"
+      case 2: return "Toma unas Fotos";
+      case 3: return "Agrega unos detalles";
       default: return `<ERROR: Paso ${this.currentStep}>`
     }
   }
 
   onGoBackClick() {
-    if (this.currentStep == 2) {
+    if (this.currentStep !== 1) {
       this.currentStep--;
     }
   }
 
   onContinueClick() {
-    if (this.currentStep == 1) {
+    if (this.currentStep !== 3) {
       this.currentStep++
     }
   }
@@ -46,11 +58,17 @@ export class ObservationPage implements OnInit {
       // Should be responsability of "PicturesService"
       const reader = new FileReader();
       reader.onload = (event: ProgressEvent) => {
-        console.log(event);
-
         this.picture = (event.target as any).result;
       };
       reader.readAsDataURL(files[0]);
     }
+  }
+
+  onAddComments() {
+    console.log('clicked');
+  }
+
+  async onFinishClick() {
+    console.log("finish");
   }
 }
