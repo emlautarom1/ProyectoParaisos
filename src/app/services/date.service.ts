6f9bb37d46f45a6bf91fc4971951c48dc6d310b7
Lautaro Martin Emanuel <emlautarom1@gmail.com>
2019-12-09 14:57:49 -0300

    Added Current Date service

---
+++ src/app/services/date.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import * as moment from "moment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DateService {
+  constructor() {
+  }
+
+  getCurrentDate(format: string = "DD-MM-YYYY"): String {
+    return moment().format(format);
+  }
+}
