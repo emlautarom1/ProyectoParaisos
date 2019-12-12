6a03a234afe0b566417d7c207aae19902297ea4b
Lautaro Martin Emanuel <emlautarom1@gmail.com>
2019-12-12 09:31:20 -0300

    Added 'under construction' component

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { TabsPageRoutingModule } from './tabs-routing.module';
 
 import { TabsPage } from './tabs.page';
+import { UnderConstructionComponent } from '@app/placeholders/under-construction/under-construction.component'
 
 @NgModule({
   imports: [
@@ -14,6 +15,9 @@ import { TabsPage } from './tabs.page';
     FormsModule,
     TabsPageRoutingModule
   ],
-  declarations: [TabsPage]
+  declarations: [
+    TabsPage,
+    UnderConstructionComponent
+  ]
 })
-export class TabsPageModule {}
+export class TabsPageModule { }
