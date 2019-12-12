1bc9b824da246f8127d22b0151c48c4324b1cdcc
Lautaro Martin Emanuel <emlautarom1@gmail.com>
2019-12-12 15:20:01 -0300

    Added upload service

---
+++ src/app/services/upload.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorageReference, AngularFireStorage } from '@angular/fire/storage';
+import { UploadTask, UploadTaskSnapshot } from '@angular/fire/storage/interfaces';
+
+import { UUID } from '@app/utils/uuid';
+import { Observation, ObservationDTO } from '@app/models/observation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadService {
+  private obsRef: AngularFirestoreCollection;
+  private picRef: AngularFireStorageReference;
+
+  constructor(
+    private db: AngularFirestore,
+    private storage: AngularFireStorage
+  ) {
+    this.picRef = this.storage.ref('fotos');
+    this.obsRef = this.db.collection('observaciones');
+  }
+
+  private async storeObservation(dto: ObservationDTO) {
+    await this.obsRef.add(dto);
+  }
+
+  private async storePicture(picture: File) {
+    const uuid = UUID();
+    const extension = picture.name.split('.').pop();
+    const fullName = `${uuid}.${extension}`;
+
+    const ref = this.picRef.child(fullName);
+    const uploadTask: UploadTask = ref.put(picture);
+    const snap: UploadTaskSnapshot = await uploadTask;
+    return snap.metadata.fullPath;
+  }
+
+  async uploadObservation(observation: Observation, pictures: File[]) {
+    const urls = await Promise.all(
+      pictures.map(pic => this.storePicture(pic))
+    );
+    const dto: ObservationDTO = { obs: observation, pictures: urls };
+    await this.storeObservation(dto);
+  }
+}
