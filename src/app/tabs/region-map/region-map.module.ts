cd9d4b352b9a99424c5e739f5330208f004fd31c
Lautaro Martin Emanuel <emlautarom1@gmail.com>
2019-12-12 16:44:09 -0300

    Added 'placeholder' module imports

--- src/app/tabs/region-map/region-map.module.ts
+++ src/app/tabs/region-map/region-map.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { RegionMapPage } from './region-map.page';
+import { PlaceholdersModule } from '@app/placeholders/placeholders.module';
 
 const routes: Routes = [
   {
@@ -19,7 +20,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    PlaceholdersModule
   ],
   declarations: [RegionMapPage]
 })
