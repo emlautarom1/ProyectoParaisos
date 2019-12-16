f7d21eaaf1333f964026a41ee84c0e34a245e36a
Lautaro Martin Emanuel <emlautarom1@gmail.com>
2019-12-16 16:03:09 -0300

    Added picture service with compression

---
+++ src/app/services/picture.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import Compress from 'client-compress';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PictureService {
+  private compress = new Compress({
+    quality: 1
+  })
+
+  constructor() { }
+
+  async compressFile(file: File) {
+    const conversions = await this.compress.compress([file]);
+    const { photo, info } = conversions[0];
+    console.log(info);
+    const url = URL.createObjectURL(photo.data);
+    return { photo, url };
+  }
+}
