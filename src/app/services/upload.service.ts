ab56040ec3083ab95b9588654700021716c2a03e
Lautaro Martin Emanuel <emlautarom1@gmail.com>
2019-12-16 16:38:03 -0300

    Now works with FormPictures

--- src/app/services/upload.service.ts
+++ src/app/services/upload.service.ts
@@ -5,6 +5,7 @@ import { UploadTask, UploadTaskSnapshot } from '@angular/fire/storage/interfaces
 
 import { UUID } from '@app/utils/uuid';
 import { Observation, ObservationDTO } from '@app/models/observation';
+import { FormPicture } from '@app/models/form-picture';
 
 @Injectable({
   providedIn: 'root'
@@ -25,18 +26,18 @@ export class UploadService {
     await this.obsRef.add(dto);
   }
 
-  private async storePicture(picture: File) {
+  private async storePicture(picture: FormPicture) {
     const uuid = UUID();
     const extension = picture.name.split('.').pop();
     const fullName = `${uuid}.${extension}`;
 
     const ref = this.picRef.child(fullName);
-    const uploadTask: UploadTask = ref.put(picture);
+    const uploadTask: UploadTask = ref.put(picture.data);
     const snap: UploadTaskSnapshot = await uploadTask;
     return snap.metadata.fullPath;
   }
 
-  async uploadObservation(observation: Observation, pictures: File[]) {
+  async uploadObservation(observation: Observation, pictures: FormPicture[]) {
     const urls = await Promise.all(
       pictures.map(pic => this.storePicture(pic))
     );
